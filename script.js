c9d6c0c7676fbd7b5a51804cca3821a3050d6cf6
davinci8gilbert <105763346+davinci8gilbert@users.noreply.github.com>
2022-08-13 21:28:39 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function Zoomout(n) {
+    document.getElementsByName("image")[n].style.transform ="scale"+"(1.5)";
+    document.getElementsByName("image")[n].style.transition =".3s";
+}
+function Zoomin(n) {
+    document.getElementsByName("image")[n].style.transform ="scale"+"(1)"; 
+}
+
+    
+
+
